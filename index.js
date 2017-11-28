6e1f3a862155e59545b946515a437ec19bbccf33
Dakota Lillie <dakota.lillie@icloud.com>
2017-11-27 20:50:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-const app = "I don't do much.";
+const token = '565995a9417224e3f8c5dcaacf0addc945c240e8'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json())
+.then(json => console.log(json));
